beb20d7703ba43fc04135c72e92a00fec316516b
Daniel Corson <danl@alum.mit.edu>
2009-06-05 20:23:43 -0700

    take path args; type sigs

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
--- run the unix du utility, recursing into large subdirectories, 
+-- run the unix du utility, recursing into large subdirectories,
 -- to find large files
 
 -- TODO:
@@ -8,25 +8,46 @@
 -- - check behavior on links
 -- - try to support "show top N"-like functionality?
 
+import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import Data.List
-import qualified Data.Map as M
+import FUtil
+import System.Console.GetOpt
 import System.Environment
-import System.Process
 import System.IO
+import System.Process
+import qualified Data.Map as M
 
 data Du = Du (Int, String) [Du] deriving Show
 data IOList a = Empty | IOValue a (IO (IOList a))
 
--- in K
-defDescendSize = 1000000
-defMinSize = 1000
+data Options = Options {
+  optFileMinSize :: Int,
+  optDirMinSize :: Int
+}
+
+-- in KB
+defOpts :: Options
+defOpts = Options {
+  optFileMinSize = 1000,
+  optDirMinSize = 1000
+}
+
+options :: [OptDescr (Options -> Options)]
+options = [
+  Option "f" ["file-min-size"]
+    (ReqArg (\ a o -> o {optFileMinSize = read a}) "KB")
+    "Only show files >= this size",
+  Option "d" ["dir-min-size"]
+    (ReqArg (\ a o -> o {optDirMinSize = read a}) "KB")
+    "Only show/descend into directories >= this size"
+  ]
 
 du :: [String] -> IO [(Int, String)]
-du files = do 
+du files = do
   let duLineProc s = first read . second tail . break (== '\t') $ s
-  (_pIn, pOut, pErr, pId) <- 
+  (_pIn, pOut, pErr, pId) <-
     runInteractiveProcess "du" ("-s":files) (Just ".") Nothing
   waitForProcess pId
   -- FIXME: handle err
@@ -37,11 +58,10 @@ du files = do
 killDotSlash ('.':'/':rest) = rest
 killDotSlash rest = rest
 
-interlines = intercalate "\n"
-
+findir :: String -> IO [String]
 findir path = do
   -- FIXME: handle newlines in file names
-  (_pIn, pOut, pErr, pId) <- runInteractiveProcess "find" 
+  (_pIn, pOut, pErr, pId) <- runInteractiveProcess "find"
     [path, "-mindepth", "1", "-maxdepth", "1", "-type", "d"] (Just ".") Nothing
   -- FIXME: handle err
   c <- hGetContents pOut
@@ -49,22 +69,18 @@ findir path = do
 
 duRecurse :: Int -> Int -> (Int, String) -> IO Du
 duRecurse descendSize minSize k@(size, file) = do
-  dus <- if size >= descendSize 
+  dus <- if size >= descendSize
     then unIOList =<< dur descendSize minSize file
     else return []
   return $ Du k dus
 
+unIOList :: IOList a -> IO [a]
 unIOList Empty = return []
-unIOList (IOValue x ysIO) = do
-  ys <- ysIO
-  xs <- unIOList ys
-  return (x:xs)
+unIOList (IOValue x ysIO) = liftM (x:) (unIOList =<< ysIO)
 
 sequenceList :: [IO a] -> IO (IOList a)
 sequenceList [] = return Empty
-sequenceList (x:xs) = do
-  y <- x
-  return $ IOValue y (sequenceList xs)
+sequenceList (x:xs) = (flip IOValue $ sequenceList xs) <$> x
 
 dur :: Int -> Int -> String -> IO (IOList Du)
 dur descendSize minSize path = do
@@ -75,17 +91,14 @@ dur descendSize minSize path = do
     sequenceList $ map (duRecurse descendSize minSize) l
 
 duShow :: Int -> Du -> String
-duShow n (Du (size, file) dus) = interlines $ [replicate n ' ' ++ show size ++ 
+duShow n (Du (size, file) dus) = interlines $ [replicate n ' ' ++ show size ++
   "\t" ++ killDotSlash file] ++ map (duShow (n + 1)) dus
 
+main :: IO ()
 main = do
-  args <- getArgs
-  [descendSize, minSize] <- case length args of
-    0 -> return [defDescendSize, defMinSize]
-    1 -> return [read $ head args, defMinSize]
-    2 -> return $ map read args
-    _ -> error "usage"
-  dus <- dur descendSize minSize "."
-  -- FIXME: find a way to print lines as they are found, not all at end
-  dus' <- unIOList dus
-  putStrLn . interlines . map (duShow 0) $ dus'
+  (opts, args) <- doArgs "usage" defOpts options
+  flip mapM_ (if null args then ["."] else args) $ \ path -> do
+    dus <- dur (optDirMinSize opts) (optFileMinSize opts) path
+    -- FIXME: find a way to print lines as they are found, not all at end
+    dus' <- unIOList dus
+    putStrLn . interlines $ map (duShow 0) dus'
